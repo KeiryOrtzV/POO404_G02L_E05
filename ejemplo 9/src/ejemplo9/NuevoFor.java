f365682d2b3c79fd2a20e10082f7f5e1078730d0
Adriel-Montano <ml232940@alumno.udb.edu.sv>
2024-07-15 23:23:27 -0600

    Add files via upload

---
+++ ejemplo 9/src/ejemplo9/NuevoFor.java
@@ -0,0 +1,24 @@
+package ejemplo9;
+
+public class NuevoFor {
+/**
+ * @param args the command line arguments
+ */
+public static void main(String[] args) {
+// TODO code application logic here
+    int Numero[]={1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    int Suma=0;
+//for clasico
+    for(int indice=0; indice<10; indice++){
+        Suma=Suma + Numero[indice];
+    }
+    System.out.println("Total con for clasico " + Suma);
+    Suma=0;
+    System.out.println("Suma reinicializada vale " + Suma);
+//for mejorado
+    for(int valor: Numero){
+        Suma += valor;
+    }
+    System.out.println("Total con for mejorado " + Suma);
+}
+}
